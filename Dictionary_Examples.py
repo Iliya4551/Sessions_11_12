cad32750d54fb691978ad4d7968ad598ea5f2a2b
iliyachaaban <ichaaban.ieu2023@student.ie.edu>
2025-03-10 21:30:26 +0100

    Sessions 11-12
    Made public now

--- Dictionary_Examples.py
+++ Dictionary_Examples.py
@@ -1,29 +1,31 @@
-d = {} #this is an empty dictionary
+d = {} # empty dict
 eng_to_spa = {"one": "uno", "two": "dos", "three": "tres"}
 print(eng_to_spa)
-eng_to_spa["i"] = "yo"
-eng_to_spa["am"] = "soy"
-eng_to_spa["spanish"] = "español"
+print(eng_to_spa["three"])
+eng_to_spa["pineapple"] = "pina" # you can add to the dict
+print(eng_to_spa["pineapple"])
+eng_to_spa.update({"yes": "si", "no": "no", "i": "yo", "am": "soy",
+                   "cuban": "cubano"})
 print(eng_to_spa)
-sentence = "i am spanish"
+print(f"i know {len(eng_to_spa)} spanish words")
+sentence = "i am cuban"
 words = sentence.split()
+translation = ""
 for word in words:
-    print(eng_to_spa[word])
-eng_to_spa.update({"yes": "si", "no": "no"}) #this lets you update your dict. with a new dict.
+    translation += eng_to_spa[word]+" "
+print(f"{sentence} translates to: {translation}")
+print(list(eng_to_spa.values()))
+print(list(eng_to_spa.keys()))
+x = eng_to_spa.pop("pineapple")
+print(x)
 print(eng_to_spa)
-del eng_to_spa["no"] #that is how you remove a key/value from dict.
+
+if "car" in eng_to_spa:
+    print(eng_to_spa["car"])
+else:
+    print("i dont know this word")
+
+print(eng_to_spa.get("car", "sorry dont know"))
+eng_to_spa.setdefault("one", "sorry dont know")
+eng_to_spa.setdefault("monkey", "sorry dont know")
 print(eng_to_spa)
-
-print(eng_to_spa.popitem()) #popitem takes out the last item that was added. (not useful though because it is hard to know which is the last item you put.
-
-print(eng_to_spa.pop("two")) #better to pop an item than popitem it because you specify the key
-
-if "tree" in eng_to_spa:
-    print(eng_to_spa["tree"])
-else: #basically gives a command incase the "if" function doesn't work
-    print("I don't know what that is blud")
-
-print(eng_to_spa("tree", "unknown word"))
-
-for key in eng_to_spa:
-    print(f"{eng_to_spa[key]} means {key}")
--- Multiple_of_6.py
+++ Multiple_of_6.py
@@ -1,17 +1,19 @@
-def get_multiple_6():
+def multiple_of_6():
     """
-    Returns a multiple of 6 that was entered by the user
-    :return: int a number
+    Returns a multiple of 6, keeps asking otherwise
+    :return: int
     """
-    try:
-        n = input("Please give me a multiple of 6: ")
-        n = int(n)
-        #How do we check if something is a multiple of 6?
-        if n % 6 == 0:
-            return n
-        else:
-            print("that's not a multiple of 6")
-    except ValueError:
-        print("you have not entered a number")
+    while True:
+        try:
+            n = input("Please give me a multiple of 6: ")
+            n = int(n)
 
-get_multiple_6()
+            if n % 6 == 0:
+                return n
+            else:
+                print("that is not a multiple of 6")
+            # if n / 6 == n // 6
+        except ValueError:
+            print("that is not a valid number")
+
+multiple_of_6()
